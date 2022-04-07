7c970a64de03640a12059c9fc867dc594c523558
Helcio <helcio@carmo.in>
2022-04-07 20:39:18 -0300

    fix: Corrige AgendaHistoricoRepository

--- src/main/java/br/uniamerica/consultorio/repository/AgendaHistoricoRepository.java
+++ src/main/java/br/uniamerica/consultorio/repository/AgendaHistoricoRepository.java
@@ -1,9 +1,9 @@
 package br.uniamerica.consultorio.repository;
 
-import br.uniamerica.consultorio.entity.Agenda;
+import br.uniamerica.consultorio.entity.AgendaHistorico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AgendaHistoricoRepository extends JpaRepository<AgendaRepository, Long> {
+public interface AgendaHistoricoRepository extends JpaRepository<AgendaHistorico, Long> {
 }
