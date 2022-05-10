c815e28d1f799b12899f12dbcb21e303b38fd6fa
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-05-10 17:54:43 -0300

    feat: Checagens agendamento

--- src/main/java/br/uniamerica/consultorio/service/AgendaService.java
+++ src/main/java/br/uniamerica/consultorio/service/AgendaService.java
@@ -1,6 +1,7 @@
 package br.uniamerica.consultorio.service;
 
 import br.uniamerica.consultorio.entity.Agenda;
+import br.uniamerica.consultorio.entity.StatusAgendamento;
 import br.uniamerica.consultorio.repository.AgendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -25,15 +26,19 @@ public class AgendaService {
     }
 
     public void update(Long id, Agenda agenda) {
-        if (id == agenda.getId()) {
-            this.saveTransactional(agenda);
-        } else {
-            throw new RuntimeException();
+        if (this.hasValidStatus(agenda)) {
+            if (id == agenda.getId()) {
+                this.saveTransactional(agenda);
+            } else {
+                throw new RuntimeException();
+            }
         }
     }
 
     public void insert(Agenda agenda) {
-        this.saveTransactional(agenda);
+        if (this.hasValidStatus(agenda)) {
+            this.saveTransactional(agenda);
+        }
     }
 
     @Transactional
@@ -49,5 +54,21 @@ public class AgendaService {
                     agenda.getId()
             );
         }
+    }
+
+    public boolean hasValidStatus(Agenda agenda) {
+        if (agenda.getStatusAgendamento().equals(StatusAgendamento.Compareceu) &&
+                agenda.getDataDe().compareTo(LocalDateTime.now()) > 0) {
+            return false;
+        }
+        else if (agenda.getStatusAgendamento().equals(StatusAgendamento.NaoCompareceu) &&
+                agenda.getDataDe().compareTo(LocalDateTime.now()) > 0) {
+            return false;
+        }
+        else if (agenda.getStatusAgendamento().equals(StatusAgendamento.Pendente) &&
+                (agenda.getEncaixe() || agenda.getDataDe().compareTo(LocalDateTime.now()) < 0)) {
+            return false;
+        }
+        return true;
     }
 }
